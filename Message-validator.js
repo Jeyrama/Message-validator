25775266ce8ee1d6b618ebbceaeaab589d87676a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-24 17:46:58 -0800

    Update Message-validator.js

--- Message-validator.js
+++ Message-validator.js
@@ -18,3 +18,23 @@ Notes:
 
 
 // Solution
+
+function isAValidMessage(message){
+  if (!message) return true
+
+  while (message.length > 0) {
+    let num = parseInt(message);
+  
+    if (isNaN(num)) return false;
+    
+    message = message.slice((num + "").length);
+    
+    let str = message.slice(0, num);
+    let regex = RegExp('^[a-zA-Z]{' + num + '}$')
+    
+    if (!regex.test(str)) return false;
+    
+    message = message.slice(num);
+  }
+  return true;
+}
